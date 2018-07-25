2695585a578e9b32b8bb12f7d83bd91570a0c97c
meverone <meverone@gmail.com>
2018-07-25 21:36:12 +0200

    Fixed sentry issue with New Relic

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -6,5 +6,11 @@ from django.utils.text import force_text
 
 def adapt(text):
     a = psycopg2.extensions.adapt(force_text(text))
-    a.prepare(connection.connection)
+    c = connection.connection
+
+    # This is a workaround for:
+    # https://szeryf.mvpl.eu/sentry/mv-demo/issues/7946/
+    if hasattr(c, '__wrapped__'):
+          c = getattr(c, '__wrapped__')
+    a.prepare(c)
     return a
